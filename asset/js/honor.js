09e16ea2866e4ce3fee4aa80e23fb51b12e763aa
Xuanang Zhao <Sd.Invol@gmail.com>
2018-05-20 23:30:37 +0800

    Add sample table

--- asset/js/honor.js
+++ asset/js/honor.js
@@ -7,3 +7,33 @@ $("#sidebar").affix({
       top: 60
     }
 });
+
+function render(result) {
+
+}
+
+
+$(document).ready(function () {
+    let years = [];
+    for (let year in honor) {
+        years.push(year);
+    }
+    years = years.sort().reverse();
+
+    for (let year of years) {
+        const detail = honor[year];
+
+        let section = $(`<section id="${year}"><h2>${year}</h2></section>`);
+
+        if (detail.some((contest) => contest.type == 0)) {
+            let WF = $(`<div id="${year + "_WF"}"><h3>World Finals</h3></div>`);
+            let result = detail.find((contest) => contest.type == 0);
+            //WF.append(render(result));
+            section.append(WF);
+        }
+
+        $('#honor').append(section);
+    }
+
+});
+
