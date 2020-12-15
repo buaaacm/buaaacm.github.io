d4f485a870c8ef8d8c5b10ee907ec58400c626bd
zhongzihao <642524394@qq.com>
2020-12-15 17:17:03 +0800

    update honor

--- asset/js/honor.js
+++ asset/js/honor.js
@@ -8,32 +8,20 @@ $("#sidebar").affix({
     }
 });
 
-function awardString(x) {
-    if (x == 1) return "金奖 (冠军)";
-    if (x == 2) return "金奖 (亚军)";
-    if (x == 3) return "金奖 (季军)";
-    if (x == 4) return "金奖";
-    if (x == 5) return "银奖";
-    if (x == 6) return "铜奖";
-    if (x == 7) return "优胜奖";
-    return `第${-x}名`;
-}
-
 function renderTable(result) {
     let table = $('<table class="table table-bordered"></table>');
     let thead = $('<thead><colgroup><col width="25%"></col><col width="20%"></col><col width="20%"></col><col width="20%"></col>' +
         '<col width="15%"></col></colgroup><tr><th>队伍</th><th colspan="3">成员</th><th>奖项</th></tr></thead>');
     table.append(thead);
     let tbody = $('<tbody></tbody>');
-    for (let team of result.honor) {
-        let id = team[0];
+    for (let team of result) {
         let tr = $('<tr></tr>');
-        tr.append(`<td>${teams[id].cn || teams[id].en}</td>`);
-        for (let i = 0; i < 3; ++i) {
-            let member = teams[id].members[i];
-            tr.append(`<td>${members[member].name}</td>`);
+        tr.append(`<td>${team.name || team.english_name}</td>`);
+        for (let i = 1; i <= 3; ++i) {
+            name = team[`member${i}`]
+            tr.append(`<td>${name}</td>`);
         }
-        tr.append(`<th>${awardString(team[1])}</th>`);
+        tr.append(`<th>${team.award}</th>`);
         tbody.append(tr);
     }
     table.append(tbody);
@@ -42,10 +30,19 @@ function renderTable(result) {
 
 
 $(document).ready(function () {
-    let years = [];
-    for (let year in honor) {
-        years.push(year);
-    }
+    let honor;
+    $.ajax({
+        dataType: 'json',
+        url: 'https://api.buaaacm.com/honor/',
+        data: { },
+        type: 'GET',
+        async: false,
+        success: function(data){
+            honor = data;
+        }
+    });
+
+    let years = Object.keys(honor);
     years = years.sort().reverse();
 
     for (let year of years) {
@@ -56,31 +53,31 @@ $(document).ready(function () {
         let navi = $(`<li><a href="#${year}">${yearName}</a></li>`);
         let subnavi = $('<ul class="nav nav-stacked"></ul>');
 
-        if (detail.some((contest) => contest.type == 0)) {
+        if (detail.some((contest) => contest.info.type === 'World-Finals')) {
             let WF = $(`<div id="${year + "_WF"}"><h3>ICPC World Finals</h3></div>`);
-            let result = detail.find((contest) => contest.type == 0);
-            WF.append(`<h4>${result.region}</h4>`)
-            WF.append(renderTable(result));
+            let result = detail.find((contest) => contest.info.type === 'World-Finals');
+            WF.append(`<h4>${result.info.region}</h4>`)
+            WF.append(renderTable(result.honors));
             section.append(WF);
             subnavi.append($(`<li><a href="#${year + "_WF"}">ICPC World Finals</a></li>`));
         }
-        if (detail.some((contest) => contest.type == 1)) {
+        if (detail.some((contest) => contest.info.type === 'ICPC')) {
             let icpc = $(`<div id="${year + "_icpc"}"><h3>ICPC Regional</h3></div>`);
-            let results = detail.filter((contest) => contest.type == 1);
+            let results = detail.filter((contest) => contest.info.type === 'ICPC');
             for (let result of results) {
-                let contest = $(`<div><h4>${result.region}</h4></div>`);
-                contest.append(renderTable(result));
+                let contest = $(`<div><h4>${result.info.region}</h4></div>`);
+                contest.append(renderTable(result.honors));
                 icpc.append(contest);
             }
             section.append(icpc);
             subnavi.append($(`<li><a href="#${year + "_icpc"}">ICPC Regional</a></li>`));
         }
-        if (detail.some((contest) => contest.type == 2)) {
+        if (detail.some((contest) => contest.info.type === 'CCPC')) {
             let ccpc = $(`<div id="${year + "_ccpc"}"><h3>CCPC</h3></div>`);
-            let results = detail.filter((contest) => contest.type == 2);
+            let results = detail.filter((contest) => contest.info.type == 'CCPC');
             for (let result of results) {
-                let contest = $(`<div><h4>${result.region}</h4></div>`);
-                contest.append(renderTable(result));
+                let contest = $(`<div><h4>${result.info.region}</h4></div>`);
+                contest.append(renderTable(result.honors));
                 ccpc.append(contest);
             }
             section.append(ccpc);
